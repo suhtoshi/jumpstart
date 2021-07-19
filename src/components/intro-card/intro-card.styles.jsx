795bb00ccd3fe0be58543156b094db15986ccd7c
suhtoshi <haydenk.lewis@gmail.com>
2021-07-19 15:32:15 -0500

    added margins and adjusted intro-card box

--- src/components/intro-card/intro-card.styles.jsx
+++ src/components/intro-card/intro-card.styles.jsx
@@ -9,25 +9,32 @@ export const IntroCardContainer = styled.div`
     background-size: cover;
     display: flex;
     justify-content: flex-end;
-    padding-top: 70px;
-
+    padding-top: 140px;
 `
-
-
 export const ContentContainer = styled.div `
   height: 90px;
-  width: 40%;
+  width: 20%;
   padding-top: 30px;
   padding-left: 30px;
   padding-bottom: 30px;
+  padding-right: 30px;
+  margin-right: 120px;
   display: flex;
   align-items: center;
   justify-content: center;
   background-color: rgb(107, 78, 144);
+  opacity: 0.9;
+  @media screen and (max-width: 800px) {
+    height: 50px;
+  }
 `
 export const ContentTitle = styled.div `
   font-weight: bold;
   margin-bottom: 6px;
-  font-size: 2.2em;
+  font-size: 2em;
   color: white;
+  text-align: center;
+    @media screen and (max-width: 800px) {
+      font-size: 1.2em;
+    }
 `

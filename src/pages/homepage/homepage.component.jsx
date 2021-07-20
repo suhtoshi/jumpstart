0ef4395703ec6c216dc12a6dd5c049af327e6269
suhtoshi <haydenk.lewis@gmail.com>
2021-07-20 11:52:38 -0500

    added homepage padding to help with widescreen displays

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Header from '../../components/header/header.component';
 import IntroCard from '../../components/intro-card/intro-card.component';
 import ProductIntro from '../../components/product-intro/product-intro.component';
+import {HomePageContainer} from './homepage.styles'
 
 const HomePage = () => (
     <div className = 'homepage'>
+    <HomePageContainer>
         <Header/>
         <IntroCard/>
         <ProductIntro/>
+    </HomePageContainer>
     </div>
 )
 
---
+++ src/pages/homepage/homepage.styles.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const HomePageContainer = styled.div `
+    @media screen and (min-width: 1100px){
+    padding-right: 120px;
+    padding-left: 120px;
+}
+`

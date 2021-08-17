7cfd5fb5c2eae7600d25448c4e39d2f3031569f6
suhtoshi <haydenk.lewis@gmail.com>
2021-08-17 09:45:35 -0500

    finished google sign in header prop to change when signed in

--- src/App.js
+++ src/App.js
@@ -11,23 +11,49 @@ import JumpstartPage from './pages/jumpstart-page/jumpstart-page.component';
 import Header from './components/header/header.component';
 import SignUpSignInPage from './pages/signup-signin-page/signup-signin.component';
 import ContactPage from './pages/contact-page/contact.component';
+import {auth} from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-    <Header/>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/affirmations' component={AffirmationPage} />
-        <Route exact path='/prepare' component={PreparePage} />
-        <Route exact path='/distractions' component={DistractionsPage} />
-        <Route exact path='/brainstorm' component={BrainstormPage} />
-        <Route exact path='/what-is-jumpstart' component={JumpstartPage} />
-        <Route exact path='/signin' component={SignUpSignInPage} />
-        <Route exact path='/contact' component={ContactPage} />
-      </Switch>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+
+      console.log(user);
+    })
+  }  
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+  render( ){
+    return (
+      <div>
+      <Header currentUser={this.state.currentUser}/>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/affirmations' component={AffirmationPage} />
+          <Route exact path='/prepare' component={PreparePage} />
+          <Route exact path='/distractions' component={DistractionsPage} />
+          <Route exact path='/brainstorm' component={BrainstormPage} />
+          <Route exact path='/what-is-jumpstart' component={JumpstartPage} />
+          <Route exact path='/signin' component={SignUpSignInPage} />
+          <Route exact path='/contact' component={ContactPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {HeaderContainer, OptionsContainer, Options, HeaderText, LogoContainer} from './header.styles.jsx';
 import { ReactComponent as Logo} from '../../assets/logo.svg'
+import {auth} from '../../firebase/firebase.utils'
 
-const Header = () => (
+
+const Header = ({currentUser}) => (
     <HeaderContainer>
     <LogoContainer to ='/'>
         <Logo/>
@@ -11,7 +13,17 @@ const Header = () => (
     <OptionsContainer>
         <Options to='/what-is-jumpstart'>Explore</Options>
         <Options to='/contact'>Contact</Options>
-        <Options to='/signin'>Sign Up</Options>
+        {
+            currentUser ? (
+                <Options as='div' onClick={()=> auth.signOut()}>
+                    Sign Out
+                </Options>
+            ):(
+                <Options to='/signin'>
+                    Sign In
+                </Options>
+            )}
+        {/* <Options to='/signin'>Sign Up</Options> */}
     </OptionsContainer>
     </HeaderContainer>
 );
